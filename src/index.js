54daea71bc8531d49c7df8ea2a590092f3019fa7
Olena <pampam32121@gmail.com>
2022-10-04 02:32:39 +0300

    added Context obj and ContextProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ContextProvider } from './Context';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ContextProvider>
     <Router>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Router>
+  </ContextProvider>
 );
 
 
