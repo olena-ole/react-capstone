9f317f72696ad9d69bf15211ba729a90c3b0820a
Olena <pampam32121@gmail.com>
2022-10-06 17:53:48 +0300

    added propTypes to Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,25 +1,43 @@
 import {useState, useContext} from 'react';
 import {Context} from '../Context';
+import PropTypes from 'prop-types';
 
 function Image({className, img}) {
 
     const {toggleFavorite} = useContext(Context);
 
     const [hovered, setHovered] = useState(false);
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>;
-    const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>;
 
+    function heartIcon() {
+        if (img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>;
+        } else if (hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>;
+        }
+    };
+
+    const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+    
     return (
         <div 
             className={`${className} image-container`}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
-            <img src={img.url} className="image-grid" />
-            {heartIcon}
+            <img src={img.url} className="image-grid" alt=""/>
+            {heartIcon()}
             {plusIcon}
         </div>
     );
+};
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 };
 
 export default Image;
